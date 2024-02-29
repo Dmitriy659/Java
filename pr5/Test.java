e7efc709ae2b024a51f2ff806eec4f3ea884e0ac
Korro345 <75068712+Dmitriy659@users.noreply.github.com>
2024-02-29 12:21:54 +0300

    Add files via upload

--- pr5/Test.java
+++ pr5/Test.java
@@ -4,14 +4,14 @@ public class Test {
     public static void main(String[] args) {
         SingletonInst obj1 = SingletonInst.getInstance();
         SingletonInst obj2 = SingletonInst.getInstance();
-        System.out.println(obj1 == obj1);
+        System.out.println("Синглтон с ленивой реализацией: " + (obj1 == obj1));
 
         SingletonEnum obj3 = SingletonEnum.INSTANCE;
         SingletonEnum obj4 = SingletonEnum.INSTANCE;
-        System.out.println(obj3 == obj4);
+        System.out.println("Синглтон с enum: " + (obj3 == obj4));
 
         SingletonHolder obj5 = SingletonHolder.getInstance();
         SingletonHolder obj6 = SingletonHolder.getInstance();
-        System.out.println(obj5 == obj6);
+        System.out.println("Синглтон со статическом классов: " + (obj5 == obj6));
     }
 }
