cc1d7fa432c516bee77241cdad37d1364f0a6ea4
Korro345 <75068712+Dmitriy659@users.noreply.github.com>
2024-03-14 21:15:14 +0300

    Add files via upload

---
+++ pr9/build.gradle.kts
@@ -0,0 +1,33 @@
+﻿plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.register<Jar>("createJar") {
+    archiveFileName.set("myapp.jar")
+    destinationDirectory.set(file("$buildDir/libs"))
+    manifest {
+        attributes["Main-Class"] = "org.example.Main"
+    }
+    from(sourceSets.main.get().output)
+}
+
+tasks.register<Exec>("buildDocker") {
+    commandLine("docker", "build", "-t", "myapp", "-f", "docker/Dockerfile", ".")
+    workingDir = file(projectDir)
+}
