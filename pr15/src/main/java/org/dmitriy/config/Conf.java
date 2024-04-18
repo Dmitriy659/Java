3b4bb6b16ba0ac0d0b4aab837c44d6c3b598cf8d
Korro345 <75068712+Dmitriy659@users.noreply.github.com>
2024-04-18 17:14:40 +0300

    Delete pr15 directory

--- pr15/src/main/java/org/dmitriy/config/Conf.java
+++
@@ -1,8 +0,0 @@
-package org.dmitriy.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-class Conf {
-
-}
--- pr15/src/main/java/org/dmitriy/repository/DepartureRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dmitriy.repository;
-
-import org.dmitriy.entity.Departure;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DepartureRepository extends CrudRepository<Departure, Long> {
-}
