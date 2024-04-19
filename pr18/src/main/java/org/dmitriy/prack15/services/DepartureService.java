b34cf5403706c7d636c1813db2d68d8afeaf280b
Korro345 <75068712+Dmitriy659@users.noreply.github.com>
2024-04-19 17:04:03 +0300

    Add files via upload

---
+++ pr18/src/main/java/org/dmitriy/prack15/services/DepartureService.java
@@ -0,0 +1,42 @@
+package org.dmitriy.prack15.services;
+
+import org.dmitriy.prack15.entity.Departure;
+import org.dmitriy.prack15.repositories.DepartureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartureService {
+    private final DepartureRepository departureRepository;
+    @Autowired
+    public DepartureService(DepartureRepository departureRepository){
+
+        this.departureRepository = departureRepository;
+    }
+
+    public String addDeparture(Departure departure){
+        departureRepository.save(departure);
+        return "Запись создана";
+    }
+
+    public Departure getDeparture(Long id){
+        Departure departure = departureRepository.findById(id).orElse(null);
+        return departure;
+
+    }
+    public List<Departure> getAllDepartures(){
+        return departureRepository.findAll();
+    }
+
+    public void removeDeparture(Long id){
+        departureRepository.deleteById(id);
+    }
+
+    public List<Departure> filterDeparturesByDate(String date) {
+        return departureRepository.findDeparturesByCreationDate(date);
+    }
+
+}
