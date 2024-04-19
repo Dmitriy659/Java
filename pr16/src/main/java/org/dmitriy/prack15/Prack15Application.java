1a3dae8f3ccf19efabc025a06f1ed88c151255c9
Korro345 <75068712+Dmitriy659@users.noreply.github.com>
2024-04-19 15:25:56 +0300

    Add files via upload

---
+++ pr16/src/main/java/org/dmitriy/prack15/Prack15Application.java
@@ -0,0 +1,14 @@
+package org.dmitriy.prack15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prack15Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Prack15Application.class, args);
+    }
+
+}
---
+++ pr16/src/main/java/org/dmitriy/prack15/services/PostOfficeService.java
@@ -0,0 +1,76 @@
+package org.dmitriy.prack15.services;
+
+import jakarta.transaction.Transactional;
+import org.dmitriy.prack15.entity.Departure;
+import org.dmitriy.prack15.entity.PostOffice;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Transactional
+public class PostOfficeService {
+    private final SessionFactory factory;
+    @Autowired
+    public PostOfficeService(SessionFactory factory){
+        this.factory = factory;
+    }
+
+    public String addPost(PostOffice postOffice){
+        System.out.println(postOffice);
+        try {
+            Session session = factory.getCurrentSession();
+            session.save(postOffice);
+            return "Запись создана";
+        } catch (Exception e){
+            return e.getMessage();
+        }
+    }
+
+    public PostOffice getPost(Long id){
+        PostOffice postOffice = null;
+        try {
+            Session session = factory.getCurrentSession();
+            postOffice = session.get(PostOffice.class, id);
+        }
+        catch (Exception e){
+            return null;
+        }
+        return postOffice;
+
+    }
+    public List<PostOffice> getAllPost(){
+        try {
+            Session session = factory.getCurrentSession();
+            Query<PostOffice> query = session.createQuery("FROM PostOffice ", PostOffice.class);
+            return query.list();
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
+
+    public Boolean removePost(Long id){
+        try {
+            Session session = factory.getCurrentSession();
+            session.createQuery("DELETE FROM PostOffice WHERE id = :postofficeId")
+                    .setParameter("postofficeId", id).executeUpdate();
+        }
+        catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public void addDeparture(Long id, Departure departure) {
+        Session session = factory.getCurrentSession();
+        PostOffice postOffice = getPost(id);
+        postOffice.AddDeparture(departure);
+        session.save(departure);
+    }
+
+}
